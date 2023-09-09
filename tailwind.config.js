7ba753d09bfb8645d6417ce9fdba1551cb3b3b43
Anika Bookout <anbookout@gmail.com>
2023-09-09 14:57:16 -0700

    initial commit (wip, placeholder content)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  // theme: {
+  //   // ...
+  // },
+  plugins: [
+    require("@tailwindcss/typography"),
+    // ...
+  ],
+};
