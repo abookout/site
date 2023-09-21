4793ad030ec5483388b063bd17018d47dae15cee
Anika Bookout <anbookout@gmail.com>
2023-09-20 19:02:12 -0700

    Starting to fill in site content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html"],
-  // theme: {
-  //   // ...
-  // },
+  theme: {
+    extend: {
+      spacing: {
+        26: "6.5rem",
+      },
+    },
+  },
   plugins: [
     require("@tailwindcss/typography"),
     // ...
